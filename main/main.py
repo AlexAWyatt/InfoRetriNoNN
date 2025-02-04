c6d87abae08cf2f2e425eec229ec5695e250d6ee
Alexander Wyatt <73673900+AlexAWyatt@users.noreply.github.com>
2025-02-04 14:16:52 -0500

    main pathing changed - make note of new "processed" folder within "data"

--- main/main.py
+++ main/main.py
@@ -3,26 +3,25 @@
 #importing section
 
 import os
+from os.path import dirname
 from parser import *
 from preprocessing import *
 
-
-
 #dataset logistics - someone please check this
-relative_dir = os.getcwd()
-dataset = relative_dir + "\\data\\scifact" #this is where we will change the dataset that we use
-doc_folder_path = dataset+ '\\corpus.jsonl'
-query_folder_path = dataset+ '\\queries.jsonl'
+absolute_base_path = dirname(dirname(__file__))
+dataset = absolute_base_path + "\\data\\scifact" #this is where we will change the dataset that we use
+doc_file_path = dataset + '\\corpus.jsonl'
+query_file_path = dataset + '\\queries.jsonl'
 #to be built
-index_folder_path = '\\inverted_index.json'
-preprocessed_docs_path = '\\preprocessed_docs.json'
-preprocessed_queries_path = '\\preprocessed_queries.json'
+index_folder_path = absolute_base_path + '\\data\\processed\\inverted_index.json'
+preprocessed_docs_path = absolute_base_path + '\\data\\processed\\preprocessed_docs.json'
+preprocessed_queries_path = absolute_base_path + '\\data\\processed\\preprocessed_queries.json'
 
 print("Parsing the dataset...")
 documents=[]
 
 #getting the queries.
-queries = parse_queries_from_file(query_folder_path)
+queries = parse_queries_from_file(query_file_path)
 
 #preprocessing the documents
 if os.path.exists(preprocessed_docs_path):
@@ -30,7 +29,7 @@ if os.path.exists(preprocessed_docs_path):
     documents = load_preprocessed_data(preprocessed_docs_path)
 else:
     print("Preprocessing documents...")
-    documents = preprocess_documents(parse_documents_from_file(doc_folder_path))
+    documents = preprocess_documents(parse_documents_from_file(doc_file_path))
     save_preprocessed_data(documents, preprocessed_docs_path)
 
 #Preprocessing the queries if they have not been preprocessed yet
@@ -39,6 +38,6 @@ if os.path.exists(preprocessed_queries_path):
     queries=load_preprocessed_data(preprocessed_queries_path)
 else:
     print("Preprocessing queries...")
-    queries = preprocess_queries(parse_queries_from_file(query_folder_path))
+    queries = preprocess_queries(parse_queries_from_file(query_file_path))
     save_preprocessed_data(queries, preprocessed_queries_path)
 
